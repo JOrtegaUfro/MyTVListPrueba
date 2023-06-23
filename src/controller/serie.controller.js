fb14ceb56a308d8219c6269be6f8030bcaec6190
Claudio Churque <82532431+Xarkican@users.noreply.github.com>
2023-06-23 15:52:06 -0400

    creacion de series para usuario

--- src/controller/serie.controller.js
+++ src/controller/serie.controller.js
@@ -1,7 +1,23 @@
 import SerieModel from "../models/serie.model.js";
 
 async function createPersonalSerie(req, res) {
-    
+  try {
+    if (!req.body.nombre || !req.body.estado || !req.body.capítulos || !req.body.minutos) {
+      return res.status(400).send({ success: false, error: "Falta algún campo como nombre, estado, capítulos, minutos" });
+    }
+    await SerieModel.create({
+      userId: req.params.userId,
+      nombre: req.body.nombre,
+      portada: req.body.portada,
+      estado: req.body.estado,
+      capítulos: req.body.capítulos,
+      minutos: req.body.minutos,
+    });
+
+    res.status(201).send({ success: true });
+  } catch (err) {
+    res.status(500).send({ success: false, error: err });
+  }
 }
 
 
