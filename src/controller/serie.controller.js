0c9a3db86283c629d1bc8cb74967db78e5d14a8c
Javier Ortega <xavierortegazepeda@gmail.com>
2023-06-23 15:50:42 -0400

    funcion para editar Serie

--- src/controller/serie.controller.js
+++ src/controller/serie.controller.js
@@ -15,7 +15,22 @@ async function deletePersonalSerieById(req,res){
 
 
 async function editPersonalSerie(req,res){
-
+    const userId = req.params.useId;
+    const serieId = req.params.serieId;
+    const series = await SerieModel.find().populate("userId"&&"serieId");
+    const matchSeries= series.filter(serie => serie.userId === userId&&serie.serieId === serieId);
+    
+    try{
+    if(matchMensajes.length>0){
+        matchSeries.estado=req.body.estado;
+        await matchSeries.save();
+        return res.status(200).send({matchSeries});
+    }else{
+        return res.status(404).send({error : "No hay mensajes"});
+    }
+}catch(error){
+    return res.status(500).send({error});
+}
 
 }
 
